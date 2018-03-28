4f1a9b4bf716f420d5459b994b75e68c79d78189
sarita khanal <saritakhanal533@gmail.com>
2018-03-28 22:12:45 +0300

    express added

---
+++ public/script.js
@@ -0,0 +1,18 @@
+
+document.addEventListener("DOMContentLoaded", function(){
+
+let work = document.querySelector("listItems").value;
+let add = document.querySelector("add");
+
+  handlers = {
+
+ let workList = {"item1s"};
+
+ let add : function(work){
+  this.workList.push(work);
+  console.log(this.worklist);
+ }
+
+ }
+
+})
