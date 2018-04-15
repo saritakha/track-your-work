d998e44801d1bbdf77c0117a6986a3d710f4f73e
sarita khanal <saritakhanal533@gmail.com>
2018-04-15 10:00:25 +0300

    added basic frontend

---
+++ public/JS/script.js
@@ -0,0 +1,17 @@
+
+
+const login = document.getElementById('submit');
+
+login.addEventListener('click',  () => {
+    console.log("what happened");
+    window.location.href="home.html";
+},false);
+
+//   handlers = {
+//   addWork = (work) => {
+//   this.workList.push(work);
+//   console.log(this.worklist);
+//  }
+
+ 
+
--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')))
+//joining app to public
+app.use(express.static('public'));
 
+//listening port 3000
 app.listen(3000, () => {
 console.log('server running');
+});
+
+// //connect database 
+// mongoose.connect('mongodb://localhost/myDB');
+
+// //getModel
+// const workModels = getSchema(workModel,"Works");
+app.get('/try', (req, res) => {
+        res.redirect('home.html');
+    
 })
